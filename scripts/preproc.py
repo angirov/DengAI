635ee72e1bb2a0552fc9d87d8b475a7e4c12bfd2
Vladimir Angirov <angirov@posteo.net>
2023-02-03 01:44:13 +0100

    add comments to loading

--- scripts/preproc.py
+++ scripts/preproc.py
@@ -9,22 +9,28 @@ def encode(data, col, max_val):
 	return data
 
 def load_data():
+	# Loading
 	df = pd.read_csv('data/dengue_features_train.csv')
 	target = pd.read_csv('data/dengue_labels_train.csv')
 	test_df = pd.read_csv('data/dengue_features_test.csv')
+	# Merging features and labels
 	train_df = pd.merge(df, target,  how='inner', left_on=['city', 'year', 'weekofyear'], right_on = ['city', 'year', 'weekofyear'])
+	# Transforming to datetime and sorting 
 	train_df['date'] = pd.to_datetime(train_df['week_start_date'])
-	test_df['date'] = pd.to_datetime(test_df['week_start_date'])
+	test_df['date']  = pd.to_datetime(test_df['week_start_date'])
 	train_df.sort_values(by= ['city', 'date'], ascending=[False, True])
 	test_df.sort_values(by= ['city', 'date'], ascending=[False, True])
+	# Missing values
 	train_df = train_df.bfill()
 	test_df = test_df.bfill()
+	# Cycic encoding
 	train_df = encode(train_df, 'weekofyear', 51)
 	test_df = encode(test_df, 'weekofyear', 51)
+	# Spliting by cities
 	train_sj = train_df[train_df['city'] == 'sj']
 	train_iq = train_df[train_df['city'] == 'iq']
-	test_sj = test_df[test_df['city'] == 'sj']
-	test_iq = test_df[test_df['city'] == 'iq']
+	test_sj  = test_df[test_df['city'] == 'sj']
+	test_iq  = test_df[test_df['city'] == 'iq']
 
 	return train_sj, train_iq, test_sj, test_iq
 
